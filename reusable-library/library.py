5ad21c72f2f0f8718931ed121bafa0646fbefb10
Vermillion <tjc0609@gmail.com>
2016-04-19 23:17:19 -0700

    Adding Reusable library directory

---
+++ reusable-library/library.py
@@ -0,0 +1,4 @@
+class Ticket(object):
+    """docstring for Ticket"""
+    def __init__(self):
+        pass
---
+++ reusable-library/page.py
@@ -0,0 +1,12 @@
+class Page(object):
+    def __init__(self):
+        pass
+
+    def head(self):
+        pass
+
+    def body(self):
+        pass
+
+    def footer(self):
+        pass
