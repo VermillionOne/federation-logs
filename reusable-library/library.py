8247bb916750c31e20636491de336a820c9cfcf2
Vermillion <tjc0609@gmail.com>
2016-04-20 22:35:07 -0700

    Added EvaluateArea class / created include_ceiling property / created find_wall_area function / created find_ceiling_area function / created find_paint_volume function

--- reusable-library/library.py
+++ reusable-library/library.py
@@ -1,16 +1,16 @@
 '''
 RoomMeasurements class
     attributes:
-    - length
-    - width
-    - height
+     __length
+     __width
+     __height
 '''
 class RoomMeasurements(object):
     def __init__(self):
         # Defining the measurement values to be used to determing room square footage
-        self.length = 0
-        self.width = 0
-        self.height = 0
+        self.__length = 0
+        self.__width = 0
+        self.__height = 0
 
     # Getter for room length
     @property
@@ -24,6 +24,7 @@ class RoomMeasurements(object):
         if new_length > 0:
             # set length to new value
             self.__length = new_length
+            print self.__length
         else:
             # Otherwise, print error message and set length to 0
             print 'Error: Invalid Length Value'
@@ -41,6 +42,7 @@ class RoomMeasurements(object):
         if new_width > 0:
             # set width to new value
             self.__width = new_width
+            print self.__width
         else:
             # Otherwise, print error message and set width to 0
             print 'Error: Invalid Width Value'
@@ -58,7 +60,77 @@ class RoomMeasurements(object):
         if new_height > 0:
             # set height to new value
             self.__height = new_height
+            print self.__height
         else:
             # Otherwise, print error message and set height to 0
             print 'Error: Invalid Height Value'
             self.__height = 0
+
+'''
+EvaluateArea class
+    attributes:
+     __include_ceiling
+     __wall_area
+     __ceiling_area
+'''
+class EvaluateArea(object):
+    def __init__(self):
+        self.__include_ceiling = False
+
+    # Getter for __include_ceiling
+    @property
+    def include_ceiling(self):
+        return self.__include_ceiling
+    # Setter for __include_ceiling
+    @include_ceiling.setter
+    def include_ceiling(self, new_ceiling):
+        # if ceiling area is included
+        if new_ceiling == True:
+            # set __include_ceiling to True
+            self.__include_ceiling = True
+        else:
+            # Otherwise, set __include_ceiling to False
+            self.__include_ceiling = False
+
+    '''
+    find_wall_area function
+        Arguments accepted:
+        from RoomMeasurements()
+            room.length ==> x_length
+            room.width  ==> y_width
+            room.height ==> z_height
+    '''
+    def find_wall_area(self, x_length, y_width, z_height):
+        if x_length > 0 and y_width > 0 and z_height > 0:
+            return 2*(x_length * z_height) + 2*(y_width * z_height)
+        else:
+            return "Invalid Values"
+
+    '''
+    find_ceiling_area function
+        Arguments accepted:
+        from RoomMeasurements()
+            room.length ==> x_length
+            room.width  ==> y_width
+
+    '''
+    def find_ceiling_area(self, x_length, y_width, ceiling):
+        if x_length > 0 and y_width > 0 and ceiling == True:
+            return x_length * y_width
+        else:
+            return "Invalid Values"
+
+    '''
+    find_finish_volume function
+        Arguments accepted:
+        surface area ==> area
+        paint volume ==> volume
+    '''
+    def find_paint_volume(self, area, volume):
+        print area, '==> area 1'
+        area = float(area)
+        print area, '==> area 2'
+        if area > 0 and volume > 0:
+            return area / volume
+        else:
+            return "Invalid Values"
--- reusable-library/main.py
+++ reusable-library/main.py
@@ -1,11 +1,36 @@
 import webapp2
-from library import RoomMeasurements
+from library import RoomMeasurements, EvaluateArea
 from page import Page
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
+        self.wall_area = 0
 
+        # Set classes for use in MainHandler
+        room = RoomMeasurements()
+        evaluate = EvaluateArea()
 
+        room.length = 15
+        room.width  = 9
+        room.height = 17
+
+        evaluate.include_ceiling = True
+
+        self.wall_area = evaluate.find_wall_area(room.length, room.width, room.height)
+        self.ceiling_area = evaluate.find_ceiling_area(room.length, room.width, evaluate.include_ceiling)
+        print self.ceiling_area, "==> ceiling area"
+        self.wall_primer_volume = evaluate.find_paint_volume(self.wall_area, 200)
+        print self.wall_primer_volume
+        self.wall_finish_volume = evaluate.find_paint_volume(self.wall_area, 350)
+        print self.wall_finish_volume
+
+        self.ceiling_primer_volume = evaluate.find_paint_volume(self.ceiling_area, 200)
+        print self.ceiling_primer_volume
+        self.ceiling_finish_volume = evaluate.find_paint_volume(self.ceiling_area, 350)
+        print self.ceiling_finish_volume
+
+        print self.wall_area
+        print self.ceiling_area
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
