d619f826e10f144354e33eb1086a66754f1ba8a0
Vermillion <tjc0609@gmail.com>
2016-04-29 13:46:46 -0700

    Added self.header to page.py

--- dynamic-website/page.py
+++ dynamic-website/page.py
@@ -1,1 +1,39 @@
-page.py
+class Page(object):
+    """docstring for Page"""
+    def __init__(self):
+        self.header = '''<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <script type="text/javascript" src="js/app.js"></script>
+
+  <link href='https://fonts.googleapis.com/css?family=BenchNine:400,700' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="css/app.css">
+</head>
+  <body>
+
+<header>
+
+  <svg class="top-left" version="1.1" x="0px" y="0px" width="222px" height="84px">
+    <path fill="#FF9900" d="M63.9,0C28.6,0,0,28.6,0,63.9V84h181v-5c0-17.1,13.9-31,31-31h10V0H63.9z"/>
+  </svg>
+
+  <div class="header-bar"></div>
+
+  <h1 class="main-header">{self.header}</h1>
+
+  <svg class="top-right" version="1.1" x="0px" y="0px" width="27px" height="48px">
+    <path fill="#FF9900" d="M3,0H0v48h3c13.3,0,24-10.7,24-24v0C27,10.7,16.3,0,3,0z"/>
+  </svg>
+
+</header>
+        '''
+
+class ContentPage(Page):
+    """docstring for ContentPage"""
+    def __init__(self, arg):
+        super(ContentPage, self).__init__()
+        self.arg = arg
