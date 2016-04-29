76ec52e63d0a1269c7fc3fd9e04bc9eea67af53a
Vermillion <tjc0609@gmail.com>
2016-04-29 13:48:38 -0700

    Added self.footer to page.py

--- dynamic-website/page.py
+++ dynamic-website/page.py
@@ -15,21 +15,37 @@ class Page(object):
 </head>
   <body>
 
-<header>
+    <header>
 
-  <svg class="top-left" version="1.1" x="0px" y="0px" width="222px" height="84px">
-    <path fill="#FF9900" d="M63.9,0C28.6,0,0,28.6,0,63.9V84h181v-5c0-17.1,13.9-31,31-31h10V0H63.9z"/>
-  </svg>
+      <svg class="top-left" version="1.1" x="0px" y="0px" width="222px" height="84px">
+        <path fill="#FF9900" d="M63.9,0C28.6,0,0,28.6,0,63.9V84h181v-5c0-17.1,13.9-31,31-31h10V0H63.9z"/>
+      </svg>
 
-  <div class="header-bar"></div>
+      <div class="header-bar"></div>
 
-  <h1 class="main-header">{self.header}</h1>
+      <h1 class="main-header">{self.header}</h1>
 
-  <svg class="top-right" version="1.1" x="0px" y="0px" width="27px" height="48px">
-    <path fill="#FF9900" d="M3,0H0v48h3c13.3,0,24-10.7,24-24v0C27,10.7,16.3,0,3,0z"/>
-  </svg>
+      <svg class="top-right" version="1.1" x="0px" y="0px" width="27px" height="48px">
+        <path fill="#FF9900" d="M3,0H0v48h3c13.3,0,24-10.7,24-24v0C27,10.7,16.3,0,3,0z"/>
+      </svg>
 
-</header>
+    </header>
+        '''
+        self.footer = '''
+
+    <footer>
+      <svg class="bottom-left" version="1.1" x="0px" y="0px" width="222px" height="43px">
+        <path fill="#FF9900" d="M181,7V0H0v15.8C0,30.8,12.2,43,27.2,43H222V19h-29C186.4,19,181,13.6,181,7z"/>
+      </svg>
+
+      <div class="footer-bar"></div>
+
+      <div class="bottom-right"></div>
+    </footer>
+
+
+  </body>
+</html>
         '''
 
 class ContentPage(Page):
